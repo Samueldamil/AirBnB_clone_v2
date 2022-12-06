a8a595abc276b2e5756e2862e8cd9df2be2b93b7
Samueldamil <damilareedun@gmail.com>
2022-12-06 10:11:24 +0000

    file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            fs_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    fs_dict[key] = value
+            return fs_dict
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +30,23 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """delete obj from __objects"""
+        if (obj is None):
+            return
+        if (obj):
+            del(obj)
+        # if (obj is None):
+        #     return
+
+        # key = obj.__class__.__name__ + '.' + obj.id
+
+        # try:
+        #     del(self.all()[key])
+        #     self.save()
+        # except KeyError:
+        #     print("** no instance found **")
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -48,3 +71,7 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def close(self):
+        """ close method """
+        self.reload()

620764186becd336938da98a688b20156a17066d
Samueldamil <damilareedun@gmail.com>
2023-01-14 20:39:44 +0000

    file

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,20 +9,20 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-	"""returns Hello HBNB!"""
-	return 'Hello HBNB!'
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	"""returns HBNB"""
-	return 'HBNB'
+    """returns HBNB"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-	"""displays C followed by value of the variable"""
-	return 'C ' + text.replace('_', ' ')
+    """display C followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000')
